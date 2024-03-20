657bdfcff181b6b218523ac8744507ad6fdff39d
Kate <wimbleimble@protonmail.com>
2024-03-20 00:14:44 +0000

    web front end

--- components/web_server/web_server.c
+++ components/web_server/web_server.c
@@ -1,18 +1,61 @@
 #include "web_server.h"
+#include "esp_http_server.h"
 #include "esp_log.h"
 
 static const char* TAG = "WEB SERVER";
+static bool current_state = false;
 
-esp_err_t get_handler(httpd_req_t* req)
+esp_err_t get_root(httpd_req_t* req)
 {
+    extern const uint8_t html_start[] asm("_binary_index_html_gz_start");
+    extern const uint8_t html_end[] asm("_binary_index_html_gz_end");
+    const size_t html_size = (html_end - html_start);
     ESP_LOGI(TAG, "BOOP");
-    const char resp[] = "BOOP\n";
-    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
+    ESP_LOGI(TAG, "Header set");
+    httpd_resp_send(req, (char*)html_start, html_size);
+    ESP_LOGI(TAG, "Sent response.");
     return ESP_OK;
 }
 
-httpd_uri_t root = {
-    .uri = "/", .method = HTTP_GET, .handler = &get_handler, .user_ctx = NULL
+esp_err_t get_state(httpd_req_t* req)
+{
+    httpd_resp_send(req, current_state ? "1" : "0", HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+esp_err_t set_state(httpd_req_t* req)
+{
+    char new_state;
+
+    int ret = httpd_req_recv(req, &new_state, 1);
+    if (ret <= 0)
+    {
+        if (ret == HTTPD_SOCK_ERR_TIMEOUT)
+        {
+            httpd_resp_send_408(req);
+            return ESP_OK;
+        }
+        return ESP_FAIL;
+    }
+    ESP_LOGI(TAG, "new state: %c", new_state);
+    
+    current_state = new_state == '1';
+
+    httpd_resp_send(req, current_state ? "1" : "0", HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+httpd_uri_t endpoint_root = {
+    .uri = "/", .method = HTTP_GET, .handler = &get_root, .user_ctx = NULL
+};
+
+httpd_uri_t endpoint_get_state = {
+    .uri = "/get_state", .method = HTTP_GET, .handler = &get_state, .user_ctx = NULL
+};
+
+httpd_uri_t endpoint_set_state = {
+    .uri = "/set_state", .method = HTTP_POST, .handler = &set_state, .user_ctx = NULL
 };
 
 httpd_handle_t web_server_init(void)
@@ -23,7 +66,9 @@ httpd_handle_t web_server_init(void)
 
     ESP_ERROR_CHECK(httpd_start(&server, &config));
 
-    ESP_ERROR_CHECK(httpd_register_uri_handler(server, &root));
+    ESP_ERROR_CHECK(httpd_register_uri_handler(server, &endpoint_root));
+    ESP_ERROR_CHECK(httpd_register_uri_handler(server, &endpoint_get_state));
+    ESP_ERROR_CHECK(httpd_register_uri_handler(server, &endpoint_set_state));
     ESP_LOGI(TAG, "Successfully started web server");
     return server;
 }
---
+++ web_front_end/index.js
@@ -0,0 +1,32 @@
+window.onload = async () => {
+    const button = document.getElementById("butt");
+    const status_box = document.getElementById("status");
+    let state;
+    button.addEventListener("click", async () =>
+    {
+        await fetch("/set_state", {
+            method: "POST",
+            body: state ? "0" : "1"
+        });
+        set_state(!state);
+    });
+    function set_state(new_state)
+    {
+        if(new_state == state)
+            return;
+
+        if(new_state)
+        {
+            button.innerHTML = "Stop";
+            status_box.innerHTML = "Device is rotating.";
+        }
+        else
+        {
+            button.innerHTML = "Start";
+            status_box.innerHTML = "Device is currently stopped.";
+        }
+        state = new_state;
+    }
+    const resp = await fetch("/get_state");    
+    set_state(Boolean(Number(await resp.text())));
+}
